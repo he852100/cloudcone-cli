2628a7d5ac58df1d67bec4b16bb76058b14f8fe6
he852100 <he852100@163.com>
2019-11-26 19:25:34 +0800

    测试

--- powershell/cloudcone-cli.ps1
+++ powershell/cloudcone-cli.ps1
@@ -47,7 +47,7 @@ $nm=@("1","1024","20")
 $note=@('CPU 核心数(1-16)','RAM in MB(512-16000)','硬盘空间（GB）')
 }
 if($name -eq "reinstall"){
-Invoke-RestMethod -Uri "$uri/list/os" -Method get -Headers $headers|%{$_.__data}|out-host
+#Invoke-RestMethod -Uri "$uri/list/os" -Method get -Headers $headers|%{$_.__data}|out-host
 $post=@('os')
 $nm=@("54")
 $note=@('操作系统编号')
@@ -58,13 +58,14 @@ $nm=@(";:.-/_:-,……-","false")
 $note=@('修改密码')
 }
 if($name -eq "create"){
-Invoke-RestMethod -Uri "$uri/list/os" -Method get -Headers $headers|%{$_.__data}|out-host
+#Invoke-RestMethod -Uri "$uri/list/os" -Method get -Headers $headers|%{$_.__data}|out-host
 $note=@('主机名','CPU 核心数(1-16)','RAM in MB(512-16000)','硬盘空间（GB）','ipv4数量(1-100)','操作系统','启用 SSD (1/0)','启用专用网络 (1/0)','启用 IPv6 (on/off)')
 $post=@('hostname','cpu','ram','disk','ips','os','ssd','pvtnet','ipv6')
 $nm=@("test.com","1","1024","20","1","54","0","1","on")
 }
 if("reinstall","create","resize","reset-password" -Contains $Name){
 for($i=0;$i -lt ($post.Length);$i++){
+if ("os" -eq $post[$i]){Invoke-RestMethod -Uri "$uri/list/os" -Method get -Headers $headers|%{$_.__data}|out-host}
 $read=Read-Host $note[$i] "默认值："$nm[$i]
 $var=if($read){$read}else{$nm[$i]}
 $var1=$var1 + ("&" + $post[$i] + "=" + $nm[$i])
@@ -73,7 +74,6 @@ $var2=$var1 -replace "^."
 if("create" -notContains $Name ){
 if($instances.id -notContains $id ){
 $instances | Format-Table id,distro,created,ips|out-host
-#[int32]$id=Read-Host "请输入id"
 Write-Host "请输入" -ForegroundColor DarkBlue -NoNewline; Write-Host "id:" -ForegroundColor black -BackgroundColor White -NoNewline; [int32]$id=Read-Host;
 if($instances.id -notContains $id ){
 Write-Host -ForegroundColor Black -BackgroundColor White "你输入的id无效，请重新执行命令"
@@ -85,7 +85,6 @@ create {"/$name"}
 resize {"/$id/$name"}
 reset-password {"/$id/reset/pass"}
 }
-
 $headers = $headers += @{'Content-Type' = 'application/x-www-form-urlencoded'}
 irm $uri$swi -Method post -Headers $headers -body "$var2"|%{
 $_.status
@@ -97,12 +96,11 @@ if ($changgui -Contains $Name){
 if("list","list-os" -notContains $Name ){
 if($instances.id -notContains $id ){
 $instances | Format-Table id,distro,created,ips|out-host
-#[int32]$id=Read-Host "请输入id"
 Write-Host "请输入" -ForegroundColor DarkBlue -NoNewline; Write-Host "id:" -ForegroundColor black -BackgroundColor White -NoNewline; [int32]$id=Read-Host;
 if($instances.id -notContains $id ){
 Write-Host -ForegroundColor Black -BackgroundColor White "你输入的id无效，请重新执行命令"
 break}
-}}}
+}}
 $swi= switch ($name){
 graphs {"/$id/$name"}
 boot {"/$id/$name"}
@@ -113,9 +111,9 @@ info {"/$id/$name"}
 status {"/$id/$name"}
 list-os {"/list/os"}
 list {"/$name/instances"}}
-irm $uri$swi -Method get -Headers $headers -ErrorAction SilentlyContinue|%{
+irm $uri$swi -Method get -Headers $headers|%{
 $_.status
 $_.message
 $_.__data.instances
 $_.__data}
-}
+}}
